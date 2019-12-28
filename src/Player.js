866fb95637c34ec247a0af3ca01a4ec6ae67d0b2
patevs <ppevans11@gmail.com>
2019-12-29 11:20:24 +1300

    ✨  src/Player.js

--- src/Player.js
+++ src/Player.js
@@ -4,7 +4,6 @@
  * Resources:
  *  https://www.w3schools.com/js/js_classes.asp
  *  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
- *  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Working_with_Objects
  */
 
 'use-strict'
@@ -17,6 +16,24 @@ class Player {
     return "Your hand : " + this.hand;
   }
 }
+
+/*
+class Model extends Player {
+  constructor(hand, name) {
+    super(hand);
+    this._name = name;
+  }
+  show() {
+    return this.present() + ', name is ' + this._name;
+  }
+  get name() {
+    return this._name;
+  }
+  set name(x) {
+    this._name = x;
+  }
+}
+*/
 
 // usage:
 // player = new Player(["card0", "card1"]);
