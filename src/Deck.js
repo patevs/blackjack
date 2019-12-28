036826b779ed8ebadc84b79f40d5fd4f7f601138
patevs <ppevans11@gmail.com>
2019-12-29 12:39:26 +1300

    ✨  Deck.js

---
+++ src/Deck.js
@@ -0,0 +1,29 @@
+/**
+ *  * src/Deck.js
+ */
+
+'use-strict'
+
+/*************
+ * * IMPORTS *
+ *************/
+
+//
+
+/****************
+ * * DECK CLASS *
+ ****************/
+
+class Deck {
+  // constructor() { }
+}
+
+/*************
+ * * EXPORTS *
+ *************/
+
+module.exports = {
+  Deck
+};
+
+// EOF //
