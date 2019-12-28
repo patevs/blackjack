00ec2202edfbf3eb338ad23b141711a1048c7d13
patevs <ppevans11@gmail.com>
2019-12-29 11:55:55 +1300

    ✨  index.js

---
+++ index.js
@@ -0,0 +1,59 @@
+/**
+ *  * index.js
+ */
+
+'use strict';
+
+/*************
+ * * IMPORTS *
+ *************/
+
+// Deck-o-cards
+const doc = require('./src/deck');
+
+/***************
+ * * FUNCTIONS *
+ ***************/
+
+const playGame = () => {
+	//
+  // Shuffle deck
+  let deck = doc.randomizedDeck();
+  dealHand(deck);
+	//
+};
+
+const dealHand = (deck) => {
+  //
+  // Handle edge cases
+  // if(deck.length < 9) process.exit();
+  // Burn a card
+  deck.pop();
+  // console.log(deck.length);
+
+  let playerHand = [];
+  let dealerHand = [];
+
+  // Deal the cards
+  playerHand.push(deck.pop());
+  dealerHand.push(deck.pop());
+  playerHand.push(deck.pop());
+  dealerHand.push(deck.pop());
+
+  console.log(playerHand);
+  // console.log(playerHand[0] + " " + playerHand[1]);
+  //
+};
+
+/*****************
+ * * ENTRY POINT *
+ *****************/
+
+(() => {
+  //
+  console.log("\n --- BLACKJACK --- \n");
+  playGame();
+  //
+})();
+
+// EOF //
