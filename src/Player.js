8d890870c8b3024856ceecf2f6bec57ba23df68b
patevs <ppevans11@gmail.com>
2019-12-29 10:50:11 +1300

    ✨  src/Player.js

---
+++ src/Player.js
@@ -0,0 +1,19 @@
+/**
+ *  * src/Player.js
+ *
+ * Resources:
+ *  https://www.w3schools.com/js/js_classes.asp
+ *  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
+ *  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Working_with_Objects
+ */
+
+class Player {
+  constructor(hand) {
+    this.hand = hand;
+  }
+}
+
+// usage:
+// player = new Player(["card0", "card1"]);
+
+// EOF //
